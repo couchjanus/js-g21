ab4c443130184d8022232b812276c97e996ace93
couchjanus <couchjanus@gmail.com>
2021-01-15 16:40:01 +0200

    added unit 4

--- js/app.js
+++ js/app.js
@@ -4,12 +4,11 @@ const closeBtn = document.querySelector(".close-btn");
 const clearCart = document.querySelector(".clear-cart");
 const single = document.querySelector(".single");
 const goBack = document.querySelector('.go-back');
-function init(){
-    closeBtn.addEventListener("click", closeCart);
-    sidebarToggle.addEventListener("click", toggleCart);
-}
 
 function toggleCart() {
+    if(single.classList.contains('show-single')){
+        single.classList.remove('show-single');
+    }
     aside.classList.toggle("show");
 }
 
@@ -17,75 +16,76 @@ function closeCart() {
     aside.classList.remove("show");
 }
 
-let addToCarts = document.querySelectorAll('.add-to-cart');
-
-// console.log(addToCarts);
-
-// console.log(addToCarts.length);
-
-// for (let i = 0; i < addToCarts.length; i++) {
-//     console.log(addToCarts[i]);
-// }
-
-// for (addToCart in addToCarts) {
-//     console.log(addToCart);
-// }
-
-// for (addToCart of addToCarts) {
-//     console.log(addToCart);
-// }
-
-// addToCarts.forEach(function(addToCart){
-//     console.log(addToCart);
-// });
-
-
-// product-detail
-
-document.addEventListener("DOMContentLoaded", init);
-
-let productDetails = document.querySelectorAll('.product-detail');
-
-// productDetails.forEach(function(element) {
-//     element.addEventListener("click", function(){
-//         single.classList.add("show-single");
-//     });
-// });
-
-goBack.addEventListener('click', function(){
-    single.classList.remove("show-single");
-})
-
-
-productDetails.forEach(function(element) {
-    element.addEventListener("click", function(e){
+function createProduct(data){
+    return `
+    <div class="col-xl-3 col-lg-4 col-sm-6">
+        <div class="product text-center">
+            <div class="position-relative mb-3">
+                <a class="d-block product-detail" href="#">
+                    <img class="img-fluid w-100 product-img" src="${data.image}" alt="${data.name}">
+                </a>
+                <div class="product-overlay">
+                    <ul class="mb-0 list-inline">
+                        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i></a></li>
+                        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark add-to-cart">Add to cart</a></li>
+                        <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark product-detail"><i class="fas fa-expand"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h6> <a class="reset-anchor product-name" href="#">${data.name}</a></h6>
+            <p class="text-small text-muted">$<span class="product-price">${data.price}</span></p>
+        </div>
+    </div>
+    `;
+}
         
-        console.log(e.target.parentNode);
-        console.log(e.target.parentNode.parentNode.parentNode.parentNode);
-        console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
-
-        // let parent = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-
-        let parent = e.target.closest(".product")
-        console.log(parent);
-
-        // description__content--about
-        let price = parent.querySelector('.product-price').innerText;
-        let name = parent.querySelector('.product-name').innerText;
-        let about = `
-        <h1>${name}</h1>
-            <h2>$${price}</h2>
-            <span>get ready to step into a new world</span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur hic minus nihil obcaecati, ratione repellendus saepe sint tenetur totam ullam. Accusantium aspernatur ducimus impedit perferendis quisquam repellendus similique ullam vitae!</p>
-        `;
-        let contentAbout = document.querySelector('.description__content--about');
-        contentAbout.innerHTML = about;
-
-        // product__img
-        let image = parent.querySelector('.product-img').getAttribute('src');
-        document.querySelector('.product__img').innerHTML = `<img src="${image}" alt="">`;
-        single.classList.add("show-single");
+function makeShowcase(products) {
+    let result = '';
+    products.forEach(item => {
+        result+=createProduct(item);
     });
+    document.querySelector('.showcase').innerHTML = result;
+}
+
+function renderShowcase(){
+    let addToCarts = document.querySelectorAll('.add-to-cart');
+    // product-detail
+    let productDetails = document.querySelectorAll('.product-detail');
+    
+    goBack.addEventListener('click', function(){
+        single.classList.remove("show-single");
+    })
+    
+    productDetails.forEach(function(element) {
+        element.addEventListener("click", function(e){
+    
+            let parent = e.target.closest(".product")
+    
+            // description__content--about
+            let price = parent.querySelector('.product-price').innerText;
+            let name = parent.querySelector('.product-name').innerText;
+            let about = `
+            <h1>${name}</h1>
+                <h2>$${price}</h2>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur hic minus nihil obcaecati, ratione repellendus saepe sint tenetur totam ullam. Accusantium aspernatur ducimus impedit perferendis quisquam repellendus similique ullam vitae!</div>
+            `;
+            let contentAbout = document.querySelector('.description__content--about');
+            contentAbout.innerHTML = about;
+    
+            // product__img
+            let image = parent.querySelector('.product-img').getAttribute('src');
+            document.querySelector('.product__img').innerHTML = `<img src="${image}" alt="">`;
+            single.classList.add("show-single");
+        });
+    });
+    
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    closeBtn.addEventListener("click", closeCart);
+    sidebarToggle.addEventListener("click", toggleCart);
+
+    makeShowcase(products);
+    renderShowcase();
 });
-
-
